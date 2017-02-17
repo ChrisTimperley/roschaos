59acd3f0d299fb416ca36878540a6b7984e58f85
Jam <jamarck96@gmail.com>
2017-02-17 00:13:26 -0500

    Added basic funcionalities such as unregistration of publishers, subscribers and services. Checks if ROS is running if is not then the program exits, but it also allows the user to ignore this condition.

--- roshammer/roshammer.py
+++ roshammer/roshammer.py
@@ -3,6 +3,8 @@ import os
 import subprocess
 import sys
 import argparse
+import xmlrpclib
+import rostopic
 from subprocess import Popen, PIPE
 
 # Thrown when a called process yields a non-zero exit status
@@ -45,9 +47,28 @@ def execute(cmd, timeout=5):
             os.killpg(p.pid, signal.SIGKILL)
             raise ProcessTimedOutException()
 
+# Uses ROS Maste API (http://wiki.ros.org/ROS/Master_API) to unregister publishers (pub),
+# subscribers (sub) and services (ser). 
+def unregister(node_name, topic_service_name, task_sub_pub_ser):
+    node_name = check_format(node_name)
+    topic_service_name = check_format(topic_service_name)
+    m = xmlrpclib.ServerProxy(os.environ['ROS_MASTER_URI'])
+    MainCode, STMAIN, URI = m.lookupNode(node_name, node_name)
+    if task_sub_pub_ser == 'sub':
+        code, statusMessage, numUnregistered = m.unregisterSubscriber(node_name, topic_service_name, URI)        
+        print(statusMessage)
+    if task_sub_pub_ser == 'pub':
+        code, statusMessage, numUnregistered = m.unregisterPublisher(node_name, topic_service_name, URI)
+        print(statusMessage)
+    elif _state == 'ser':
+        code, statusMessage, numUnregistered = m.unregisterSubscriber(node_name, topic_service_name, URI)
+        print(statusMessage)
 
 # TODO: Certain "respawing" nodes will refuse to die (http://wiki.ros.org/rosnode)
 def kill_node(node_name):
+
+
+    node_name = check_format(node_name)
     print("Killing node: {}".format(node_name))
 
     # sanity check: is this a legal ROS node name? If not, we could abuse this to perform
@@ -65,19 +86,52 @@ def kill_node(node_name):
     except ProcessTimedOutException as e:
         err("failed to kill node - request timed out")
 
+# Verifies that the topic or node that is going to be processed complies with ROS guidelines.
+def check_format(_node_topic_service):
+    node_topic_service = _node_topic_service
+    if node_topic_service[0] != '/':
+        node_topic_service = '/' + _node_topic_service
+        print('Missing ' + repr('/') + ' at the begining of the node-topic. Modifying ' + _node_topic_service + ' to ' + node_topic_service)+'.'
+
+    print('')
+    return node_topic_service
+
+# Checks if rosmaster is running. 
+def check_ros_running():
+    try:
+        rostopic.get_topic_class('/rosout')
+        return True
+    except:
+        print('Unable to detect rosmaster. Is rosmaster running?')
+        return False
+
 def main():
+    ros_running = check_ros_running()
     parser = argparse.ArgumentParser()
     subparsers = parser.add_subparsers() 
     parser.add_argument('--version', action='version', version='0.0.1')
+    parser.add_argument('-i', '--ignore_rosmaster_state', action='store_true', required=False)
 
     generate_parser = subparsers.add_parser('kill-node')
     generate_parser.add_argument('name', \
                                  help='name of the ROS node that should be killed')
     generate_parser.set_defaults(func=lambda args: kill_node(args.name))
 
+    generate_parser = subparsers.add_parser('unregister')
+    generate_parser.add_argument('-t', '--type', choices=['sub', 'pub', 'ser'], required=True)
+    generate_parser.add_argument('-n', '--node', \
+                                 help='Node that handles either the publisher or the subscriber.', required=True)
+    generate_parser.add_argument('-nm', '--topic_or_service_name', \
+                                 help='Topic that is going to be modified.', required=True)
+    generate_parser.set_defaults(func=lambda args: unregister(args.node, args.topic_or_service_name, args.type))
+
     args = parser.parse_args() 
+    if (args.ignore_rosmaster_state) == False and (ros_running == False):
+        print('ROS not detected. If you still want to run try adding -i (ex. roshammer.py -i kill-node /example )')
+        exit()
     if 'func' in vars(args):
         args.func(args)
+    
 
 if __name__ == "__main__":
     main()
