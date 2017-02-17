80abe7f87cc39b3d29f03f6f5ded3d8799bdd4a5
Chris Timperley <christimperley@googlemail.com>
2017-02-16 19:02:13 -0500

    modified time limit

--- roshammer/roshammer.py
+++ roshammer/roshammer.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
+from subprocess import Popen, PIPE
+import sys
 import argparse
+
+def err(msg):
+    print("ERROR: {}".format(msg))
+    sys.exit(1)
 
 # TODO: Certain "respawing" nodes will refuse to die (http://wiki.ros.org/rosnode)
 def kill_node(node_name):
@@ -12,9 +18,23 @@ def kill_node(node_name):
     # sanity check: is ROS running?
 
     # sanity check: is there a node with the given name running?
+    
+    # TODO: although it's better to avoid sharing the same shell as the program, we do
+    #   so to avoid having to reload the catkin workspace. No big deal.
+    # NOTE: os.setsid is used to ensure we kill all children in the progress group
+    time_limit = 5.0 # seconds
+    cmd = "roskill {}".format(node_name)
+    with Popen(cmd, shell=True, stdout=PIPE, stderr=PIPE, preexec_fn=os.setsid) as p:
+        try:
+            # convert stdout and stderr to strings
+            stdout, stderr = p.communicate(timeout=time_limit)
+            stdout = str(stdout)[2:-1]
+            stderr = str(stderr)[2:-1]
+            retcode = p.returncode
 
-    # did we kill the node?
-
+        except TimeoutExpired:
+            os.killpg(p.pid, signal.SIGKILL)
+            err("failed to kill node - request timed out")
 
 def main():
     parser = argparse.ArgumentParser()
